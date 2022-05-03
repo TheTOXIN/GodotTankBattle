8915325ad29d3fec61256440db82a43ac5c78457
TheTOXIN <thetoksin@gmail.com>
2022-05-03 21:19:02 +0300

    fix tmp

--- TMP.gd
+++ TMP.gd
@@ -3,28 +3,24 @@ extends Node2D
 var t: float = 0
 var a: float = 0
 
-var v1: Vector2 = Vector2.ZERO
-var v2: Vector2 = Vector2.ZERO
-
 func _physics_process(delta):
 	t += delta * 0.4
 	a += 10 * delta
 
-	$B.position = $B.position + Vector2(sin(a),  cos(a)) * 5
+	$B.position = $B.position + Vector2(sin(a),  cos(a)) * 10
 	$TEST.position = $A.position.linear_interpolate($B.position, t)
-	
+
 	if $TEST.position >= $B.position:
 		$TEST.position = $B.position
-
-	var r = $A.rotation
-	$A.rotation += 2 * delta
-		
-	v1 = $A.position
-	v2 = $A.position + Vector2(sin(-r), cos(r)) * 150
+	
+	$A.rotation = ($B.position - $A.position).angle()
 
 	update()
 		
 func _draw():
-	draw_line(v1, v2, Color.black, 5)
+	var v1 = $A.position
+	var v2 = ($TEST.position - $A.position) + $A.position
+	
+	draw_line(Vector2.ZERO + $A.position, v2 , Color.black, 5)
 	draw_circle(v2, 10, Color.black)
 	
