23e75bc9f7bcb2126d3bf160bc577bd6b73c60ef
TheTOXIN <thetoksin@gmail.com>
2022-05-07 23:59:40 +0300

    fix bugs

--- tanks/Player.gd
+++ tanks/Player.gd
@@ -4,7 +4,6 @@ var track_trails = false
 var speed_stop = 0
 var aim_position
 
-#TODO иногда дергается если часто жать назад когда держишь газ
 func control(delta):
 	turret_aim(delta)
 	
@@ -17,25 +16,29 @@ func control(delta):
 		
 	rotation += rotation_speed * rot_dir * delta
 
-	var possible_speed = max_speed * speed_boost *  speed_road
+	var possible_speed = max_speed * speed_boost * speed_road
+	var possible_back = -1 * (possible_speed) / 2
 	
 	if Input.is_action_pressed("forward"):
 		if Input.is_action_pressed("back"): #DRIFT
 			track_trails = true
 		else: #FORWARD
 			speed = lerp(speed, possible_speed, acceleration)
-			velocity = (Vector2.RIGHT * speed).rotated(rotation)
+			
 	if Input.is_action_pressed("back"):
 		if speed > 0: #STOP
-			if speed_stop == 0: 
+			if speed_stop == 0:
 				speed_stop = speed
 			var stopper = 300 / pow(speed_stop, 2)
-#			print(stopper)
-			speed = lerp(speed, -possible_speed / 2, stopper)
+			speed = lerp(speed, possible_back, stopper)
+			if speed > 0:
+				speed 
+			track_trails = true
 		else: #BACK
-			speed = lerp(speed, -possible_speed / 2, acceleration)
-		velocity = (Vector2.RIGHT * speed).rotated(rotation)
-		track_trails = speed > 0
+			speed = lerp(speed, possible_back, acceleration)
+			pass
+		
+	velocity = (Vector2.RIGHT * speed).rotated(rotation)
 	
 	if !Input.is_action_pressed("back") and !Input.is_action_pressed("forward"):
 		velocity = (Vector2.RIGHT * speed).rotated(rotation)
@@ -51,29 +54,22 @@ func control(delta):
 		
 	if Input.is_action_pressed("restart") and Globals.debag_mode:
 		Globals.restart()
-		
+
 	position.x = clamp(position.x, $Camera2D.limit_left, $Camera2D.limit_right)
 	position.y = clamp(position.y, $Camera2D.limit_top, $Camera2D.limit_bottom)
 	
 	if track_trails:
 		emit_signal("track", self)
 
-#TODO SHORT ANGLE NOT WORK
 func turret_aim(delta):
 	var cursor = get_global_mouse_position()
-	turret_speed = 1
-	#vector between turret and cursor
 	var angle_position = cursor - $Turret.global_position
-	#rotation from allowed this frame 
-	var angle_delta = turret_speed * delta
 	
 	var a = angle_position.angle()
 	var r = $Turret.global_rotation
-	
-	#complete rotation to ship
-	var angle_turret = lerp_angle(r, a, 1.0)
-	#limit rotation to whta is allowed this frame ans set
-	$Turret.global_rotation = clamp(a, r - angle_delta, r + angle_delta)
+
+	var angle_turret = lerp_angle(r, a, turret_speed)
+	$Turret.global_rotation = angle_turret
 	
 	#set muzzle aim
 	$Turret/Aim.position.x = $Turret.global_position.distance_to(cursor) 
